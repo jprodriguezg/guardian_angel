268dc9602f15448167c67d31f3434653217b3bff
Juan Rodriguez <juanpablo.rodriguezgomez@gmail.com>
2017-06-28 00:01:56 +0200

    ibvs node

--- ibvs_controller/src/IBVS.cpp
+++ ibvs_controller/src/IBVS.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <Eigen/Dense>
 #include <string>
 #include <math.h>
 #include <ros/ros.h>
 #include <std_msgs/Empty.h>
+#include <std_msgs/Float32MultiArray.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <tf/transform_datatypes.h>
@@ -11,9 +13,8 @@
 # define PI 3.14159265358979323846
 
 // Some global variables
-
-geometry_msgs::Twist velocityMsg;
 std::vector<double> visual_features(3,0); // [u,v]
+double camera_depth = 0.0;
 
 void hasReceivedVisualFeatures(const geometry_msgs::PoseStamped::ConstPtr& msg){
 
@@ -26,19 +27,49 @@ void hasReceivedVisualFeatures(const geometry_msgs::PoseStamped::ConstPtr& msg){
 }
 
 
+Eigen::MatrixXd computeImageJacobian(std::vector<double> vf){
+
+	Eigen::MatrixXd Ji(2,6);
+	double u = vf[1];
+	double v = vf[2];
+	double Z = vf[3];
+	Ji(1,1) = -1/Z;
+	Ji(1,2) = 0.0;
+	Ji(1,3) = u/Z;
+	Ji(1,4) = u*v;
+	Ji(1,5) = -(1+ sqrt(u));
+	Ji(1,6) = v;
+	Ji(2,1) = 0;
+	Ji(2,2) = -1/Z;
+	Ji(2,3) = v/Z;
+	Ji(2,4) = (1 + sqrt(v));
+	Ji(2,5) = u*v;
+	Ji(2,6) = -u;
+
+	return Ji;
+}
+
 int main(int argc, char** argv){
 
-ros::init(argc, argv, "IBSV_controller_node");
+ros::init(argc, argv, "ibvs_controller_node");
 ros::NodeHandle nh_;
 ros::NodeHandle nhp_("~");
 ros::Rate rate(20.0);
 
 
+// Initialize varialbes
+Eigen::MatrixXd Ji(2,6);
+std_msgs::Float32MultiArray control_angles;
+
+// Create Subscribers and Publishers
 ros::Subscriber visual_tracker_sub_=nh_.subscribe("visual_tracker", 10, hasReceivedVisualFeatures);
-//ros::Publisher vel_pub_=nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+ros::Publisher control_output_pub_=nh_.advertise<std_msgs::Float32MultiArray>("ibvs_control_output", 1);
 
 	while (ros::ok()){
 
+		Ji = computeImageJacobian(visual_features);
+
+		control_output_pub_.publish(control_angles);
    	ros::spinOnce(); // if you were to add a subscription into this application, and did not have ros::spinOnce() here, your callbacks would never get called.
     	rate.sleep();
     }
