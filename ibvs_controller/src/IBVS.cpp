dc6ada124a397cc76c6e1b6d7c88dbff69530eb1
Juan Rodriguez <juanpablo.rodriguezgomez@gmail.com>
2017-06-28 11:05:11 +0200

    adding lunch files

--- ibvs_controller/src/IBVS.cpp
+++ ibvs_controller/src/IBVS.cpp
@@ -13,38 +13,43 @@
 # define PI 3.14159265358979323846
 
 // Some global variables
-std::vector<double> visual_features(3,0); // [u,v]
+std::vector<double> drone_pose(7,0), visual_features(3,0); // [u,v]
 double camera_depth = 0.0;
 
 void hasReceivedVisualFeatures(const geometry_msgs::PoseStamped::ConstPtr& msg){
 
-	// Obtaining target point info
 	visual_features[0] = msg->pose.position.x;
 	visual_features[1] = msg->pose.position.y;
 	visual_features[2] = msg->pose.position.z;
+  return;
+}
 
+void hasReceivedDronePosition(const geometry_msgs::PoseStamped::ConstPtr& msg){
+
+	drone_pose[2] = msg->pose.position.z;
   return;
 }
 
 
-Eigen::MatrixXd computeImageJacobian(std::vector<double> vf){
+
+Eigen::MatrixXd computeImageJacobian(std::vector<double> vf, std::vector<double> uav_pose){
 
 	Eigen::MatrixXd Ji(2,6);
 	double u = vf[1];
 	double v = vf[2];
-	double Z = vf[3];
+	double Z = uav_pose[3];
+	Ji(0,0) = -1/Z;
+	Ji(0,1) = 0.0;
+	Ji(0,2) = u/Z;
+	Ji(0,3) = u*v;
+	Ji(0,4) = -(1+ sqrt(u));
+	Ji(0,5) = v;
+	Ji(1,0) = 0;
 	Ji(1,1) = -1/Z;
-	Ji(1,2) = 0.0;
-	Ji(1,3) = u/Z;
+	Ji(1,2) = v/Z;
+	Ji(1,3) = (1 + sqrt(v));
 	Ji(1,4) = u*v;
-	Ji(1,5) = -(1+ sqrt(u));
-	Ji(1,6) = v;
-	Ji(2,1) = 0;
-	Ji(2,2) = -1/Z;
-	Ji(2,3) = v/Z;
-	Ji(2,4) = (1 + sqrt(v));
-	Ji(2,5) = u*v;
-	Ji(2,6) = -u;
+	Ji(1,5) = -u;
 
 	return Ji;
 }
@@ -63,14 +68,15 @@ std_msgs::Float32MultiArray control_angles;
 
 // Create Subscribers and Publishers
 ros::Subscriber visual_tracker_sub_=nh_.subscribe("visual_tracker", 10, hasReceivedVisualFeatures);
+ros::Subscriber vrep_drone_pose_sub_=nh_.subscribe("vrep_drone_pose", 10, hasReceivedDronePosition);
 ros::Publisher control_output_pub_=nh_.advertise<std_msgs::Float32MultiArray>("ibvs_control_output", 1);
 
 	while (ros::ok()){
 
-		Ji = computeImageJacobian(visual_features);
+		Ji = computeImageJacobian(visual_features,drone_pose);
 
 		control_output_pub_.publish(control_angles);
-   	ros::spinOnce(); // if you were to add a subscription into this application, and did not have ros::spinOnce() here, your callbacks would never get called.
+   		ros::spinOnce(); // if you were to add a subscription into this application, and did not have ros::spinOnce() here, your callbacks would never get called.
     	rate.sleep();
     }
 
